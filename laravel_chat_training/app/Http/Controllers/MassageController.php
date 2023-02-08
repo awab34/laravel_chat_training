e225d4b7f1618867024c510f7caab72ada697304
awab34 <awab3468@gmail.com>
2023-02-08 15:29:22 +0200

    messages  section started

--- laravel_chat_training/app/Http/Controllers/MassageController.php
+++ laravel_chat_training/app/Http/Controllers/MassageController.php
@@ -7,58 +7,30 @@ use Illuminate\Http\Request;
 
 class MassageController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\massage  $massage
-     * @return \Illuminate\Http\Response
-     */
-    public function show(massage $massage)
-    {
-        //
-    }
+    
 
     
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\massage  $massage
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(massage $massage)
+    public function store(Request $request)
     {
-        //
+        $request->validate([
+            'message' => 'required|string',
+            'chat_id' => 'required',
+        ]);
+
+        $chat = massage::create([
+            'message'=>$request->message,
+            'user_id'=>Auth::id(),
+            'chat_id'=>$request->chat_id,
+        ]);
+        return redirect()->back();
+
+    }
+
+    public function destroy($id)
+    {
+        $massage = massage::find($id);
+        $massage->delete();
+        return redirect()->back();
     }
 }
